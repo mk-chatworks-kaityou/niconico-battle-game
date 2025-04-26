8ad83bc3f24cede6c43f47e42da165917b26f283
mk-chatworks-kaityou <mk.chatworks.kaityou@gmail.com>
2025-04-27 01:48:56 +0900

    add API functions

--- api/mylist.js
+++ api/mylist.js
@@ -1,7 +1,12 @@
 export default async function handler(req, res) {
   const { mylistId } = req.query;
-  const response = await fetch(`https://export.nicovideo.jp/mylist/${mylistId}?rss=2.0`);
-  const text = await response.text();
-  res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send(text);
+
+  try {
+    const response = await fetch(`https://export.nicovideo.jp/mylist/${mylistId}?rss=2.0`);
+    const text = await response.text();
+    res.setHeader('Content-Type', 'application/xml');
+    res.status(200).send(text);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch mylist info' });
+  }
 }
