ed09bd35c5a817f16bf4300cbd277982c453d1d1
mk-chatworks-kaityou <mk.chatworks.kaityou@gmail.com>
2025-04-27 09:15:01 +0900

    fix import fetch for ESM

--- api/mylist.js
+++ api/mylist.js
@@ -1,6 +1,5 @@
 import fetch from 'node-fetch';
 
-
 export default async function handler(req, res) {
   const { mylistId } = req.query;
 
@@ -10,6 +9,7 @@ export default async function handler(req, res) {
     res.setHeader('Content-Type', 'application/xml');
     res.status(200).send(text);
   } catch (error) {
+    console.error(error); // ★エラーも出しておくと良い
     res.status(500).json({ error: 'Failed to fetch mylist info' });
   }
 }
